8e5a611054def2f142787edf7b3cf9b9ac3f36fd
godcrampy <sahilbondre@gmail.com>
2019-05-31 15:12:28 +0530

    Initialized empty home page and routes

---
+++ lib/screens/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text('together.', style: TextStyle(color: Colors.black),),
+      ),
+    );
+  }
+}
